6504e46ed783328646dc246225ab55b4e8b9a800
DarifElmehdi <46763117+DarifElmehdi@users.noreply.github.com>
2021-06-17 18:55:32 +0100

    Adding global statistics

--- src/components/Statistiques.js
+++ src/components/Statistiques.js
@@ -1,6 +1,7 @@
 /*https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest#00030720-fae3-4c72-8aea-ad01ba17adf8*/
 import React, { useState, useEffect }from "react";
 import axios from 'axios';
+import moment from 'moment';
 
 import styled from 'styled-components'
 import DataTable from 'react-data-table-component';
@@ -8,16 +9,18 @@ import { Container } from "@material-ui/core";
 
 export default function Statistiques() {    
     const [stats, setStats] = useState([])
-    
+    const [global, setGlobal] = useState([])
     useEffect(() => {
         fetch("https://api.covid19api.com/summary")
           .then(resp => resp.json())
           .then(resp => {
-            setStats(resp['Countries'])
+            setStats(resp['Countries']); setGlobal(resp['Global']);
+            
           })
       }, [])
-    console.log(stats)
-    //const data =   [{ country_name: "A", region: "", cases: "1", new_deaths: "44", deaths: "3", serious_critical: "1", active_cases: "566", total_recovered: "56", total_cases_per_1m_population: "33" }, { country_name: "B", region: "", cases: "2", new_deaths: "44", deaths: "3", serious_critical: "233", active_cases: "566", total_recovered: "56", total_cases_per_1m_population: "33" }, { country_name: "C", region: "", cases: "3", new_deaths: "44", deaths: "3", serious_critical: "233", active_cases: "566", total_recovered: "86", total_cases_per_1m_population: "33" }, { country_name: "D", region: "", cases: "4", new_deaths: "44", deaths: "3", serious_critical: "233", active_cases: "566", total_recovered: "86", total_cases_per_1m_population: "33" }]
+    function getDate(){
+        return moment().format("DD/MM/YYYY HH:mm:ss");
+    }
 
     const columns = [{
         selector: 'Country',
@@ -29,11 +32,11 @@ export default function Statistiques() {
         },
     }, {
         selector: 'TotalConfirmed',
-        name: 'Total confirmé',
+        name: 'Total confirmés',
         sortable: true,
     },{
         selector: 'NewConfirmed',
-        name: 'Nouveau confirmé',
+        name: 'Nouveau confirmés',
         sortable: true,
     },{
         selector: 'TotalDeaths',
@@ -45,7 +48,7 @@ export default function Statistiques() {
         sortable: true,
     }, {
         selector: 'TotalRecovered',
-        name: 'Total Rétablie',
+        name: 'Total Rétablies',
         sortable: true,
     }, {
         selector: 'NewRecovered',
@@ -65,8 +68,57 @@ export default function Statistiques() {
     };
 
     const filteredItems = stats.filter(item => item.Country && item.Country.toLowerCase().includes(filterText.toLowerCase()));
+    const date = getDate();
     return (
         <Container maxWidth="mx">
+            <br/>
+            <div class="row">
+                <div class="col">
+                    <div className="card shadow-md">
+                        <div className="card-body bg-info text-center">
+                            <p className="lead">Total confirmés</p>
+                            <h3 className="card-title" style={{ fontSize: '2.2em' }}>{global['TotalConfirmed']} </h3>
+                            
+                        </div>
+                        <div className="card-footer">
+                            <small>MAJ: {date}</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div className="card shadow-md">
+                        <div className="card-body bg-danger text-center">
+                            <p className="lead">Total de décès</p>
+                            <h3 className="card-title" style={{ fontSize: '2.2em' }}>{global['TotalDeaths']} </h3>
+                        </div>
+                        <div className="card-footer">
+                            <large>MAJ: {date}</large>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div className="card shadow-md">
+                        <div className="card-body bg-success text-center">
+                            <p className="lead">Total Rétablies</p>
+                            <h3 className="card-title" style={{ fontSize: '2.2em' }}>{global['TotalRecovered']} </h3>
+                        </div>
+                        <div className="card-footer">
+                            <small>MAJ: {date}</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div className="card shadow-md">
+                        <div className="card-body bg-warning text-center">
+                            <p className="lead">Nouveaux confirmés</p>
+                            <h3 className="card-title" style={{ fontSize: '2.2em' }}>{global['NewConfirmed']} </h3>
+                        </div>
+                        <div className="card-footer">
+                            <small>MAJ: {date}</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="input-group mb-3 mt-5">
                 <input class="form-control" id="search" type="text" placeholder="Chercher une pays" value={filterText} onChange={handleFilterChange} />
                 <div class="input-group-append">
